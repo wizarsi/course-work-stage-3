2672bb4640df2da58b6d62037484a4a4c672e5a3
wizarsi <>
2022-11-15 23:26:06 +0300

    Откорректировно описание данных для написание триггеров и функций

--- put_data.sql
+++ put_data.sql
@@ -1,4 +1,9 @@
-
+insert into sport_directors(name, surname, age)
+values ('Alex', 'Backhem', 25);
+insert into sport_directors(name, surname, age)
+values ('Andrew', 'Looser', 28);
+insert into sport_directors(name, surname, age)
+values ('Pavel', 'Black', 23);
 
 insert into football_leagues (name, country, count_matches)
 values ('Premier League', 'Great Britain', 100);
@@ -28,20 +33,20 @@ values ('RUB', 'EUR', 2.61);
 insert into coasts (currency_recipient, currency_sender, exchange_rate)
 values ('RUB', 'GBP', 72.14);
 
-insert into player_contracts (value, coast_id, start_date, end_date, salary, compensation_value)
-values (250000, 1, '2001-09-28', '2003-09-28', 100, 10000);
-insert into player_contracts (value, coast_id, start_date, end_date, salary, compensation_value)
-values (2500000, 2, '2001-05-28', '2003-05-28', 1000, 100000);
+insert into player_contracts (coast_id, start_date, end_date, salary, compensation_value)
+values (1, '2001-09-28', '2003-09-28', 100, 10000);
+insert into player_contracts (coast_id, start_date, end_date, salary, compensation_value)
+values (2, '2001-05-28', '2003-05-28', 1000, 100000);
 
 insert into football_clubs
-    (name, players_count, trophies_count, budget, rating)
-values ('Manchester United', 30, 10, 20000000, 40);
+    (name, players_count, trophies_count, budget, rating, sport_director)
+values ('Manchester United', 30, 10, 20000000, 40, 1);
 insert into football_clubs
-    (name, players_count, trophies_count, budget, rating)
-values ('Real Madrid', 35, 18, 150000000, 50);
+    (name, players_count, trophies_count, budget, rating, sport_director)
+values ('Real Madrid', 35, 18, 150000000, 50, 2);
 insert into football_clubs
-    (name, players_count, trophies_count, budget, rating)
-values ('Zenit', 28, 15, 15000000, 20);
+    (name, players_count, trophies_count, budget, rating, sport_director)
+values ('Zenit', 28, 15, 15000000, 20, 3);
 
 insert into players (name, surname, age, position, statistic, contract, rating, football_club)
 values ('Cristiano', 'Ronaldo', 38, 'Atacker', 1, 1, 90, 1);
@@ -57,22 +62,23 @@ values (2, 2);
 insert into players_club(player_id, club_id)
 values (3, 3);
 
-insert into sport_directors(name, surname, age,football_club)
-values ('Alex', 'Backhem', 25, 1);
-insert into sport_directors(name, surname, age,football_club)
-values ('Andrew', 'Looser', 28, 2);
-insert into sport_directors(name, surname, age,football_club)
-values ('Pavel', 'Black', 23, 3);
+insert into coaches(name, surname, age, football_club, rating, statistic)
+values ('Mourinio', 'Vendeta', 42, 1, 23, 1);
+insert into coaches(name, surname, age, football_club, rating, statistic)
+values ('Petya', 'Vasyin', 45, 2, 40, 2);
+insert into coaches(name, surname, age, football_club, rating, statistic)
+values ('Josef', 'Ket', 50, 3, 67, 3);
 
-insert into coaches(name, surname, age, football_club, rating, statistic) values ('Mourinio','Vendeta',42,1,23,1);
-insert into coaches(name, surname, age, football_club, rating, statistic) values ('Petya','Vasyin',45,2,40,2);
-insert into coaches(name, surname, age, football_club, rating, statistic) values ('Josef','Ket',50,3,67,3);
+insert into transfer_request(value, coast_id, club_from, club_to, player_id)
+values (120000, 1, 3, 2, 3);
 
-insert into transfer_request(value,coast_id,club_from,club_to,player_id) values (120000,1,3,2,3);
+insert into exchange_request(player_id, club_from, player_get, club_to)
+values (2, 2, 1, 1);
 
-insert into exchange_request(player_id, club_from, player_get, club_to) values (2,2,1,1);
-
-insert into club_league(club_id, league_id) values (1,1);
-insert into club_league(club_id, league_id) values (2,2);
-insert into club_league(club_id, league_id) values (3,3);
+insert into club_league(club_id, league_id)
+values (1, 1);
+insert into club_league(club_id, league_id)
+values (2, 2);
+insert into club_league(club_id, league_id)
+values (3, 3);
 
