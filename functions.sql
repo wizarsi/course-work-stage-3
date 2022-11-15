541971722d5e9a86085ad08f08cd346e06f23eb3
wizarsi <>
2022-11-16 00:46:37 +0300

    Создан функция budget_for_salaries

--- functions.sql
+++ functions.sql
@@ -10,3 +10,19 @@ begin
     return new;
 end
 $$ language plpgsql;
+
+create or replace function budget_for_salaries(club_id int default null) returns int as
+$$
+begin
+    if club_id is null
+    then
+        return 0;
+    end if;
+    return (select sum(player_contracts.salary) from players
+                 join player_contracts on players.contract = player_contracts.id
+                 where players.football_club = club_id);
+end
+$$ language plpgsql;
+
+select budget_for_salaries(1);
+select budget_for_salaries(3);
